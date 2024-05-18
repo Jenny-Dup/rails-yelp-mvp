4b2e805c4bf63e2d78e98fc9372e1d49bcff229f
Jennifer Du Preez <jennydupreez13@gmail.com>
2024-05-18 14:18:13 +0100

    finalised code for Yelp MCP challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all if Rails.env.development?
+
+Restaurant.create!(
+  name: "Dishoom",
+  address: "42 Electric Boulevard, London",
+  phone_number: "+321 23 45 67 89",
+  category: "chinese"
+)
+
+Restaurant.create!(
+  name: "Pizza Paradise",
+  address: "123 East street, London",
+  phone_number: "+44 23 45 67 89",
+  category: "italian"
+)
+
+Restaurant.create!(
+  name: "The River Cafe",
+  address: "Thames Wharf, London",
+  phone_number: "+44 20 73 86 4200",
+  category: "french"
+)
+
+Restaurant.create!(
+  name: "Cocotte",
+  address: "762 Fulham road, London",
+  phone_number: "+44 78 24 51 3070",
+  category: "french"
+)
+
+Restaurant.create!(
+  name: "Sushi Samba",
+  address: "1 Westminster street, London",
+  phone_number: "+44 78 35 67 9000",
+  category: "japanese"
+)
